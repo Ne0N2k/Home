2061ebbb6ecce44bc82eeb8bb247e51598077ce3
Ne0N2k <tarasov-kola@mail.ru>
2020-10-19 14:36:00 +0500

    Исправленное

---
+++ prog geometry/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x;
+            Console.WriteLine("Введите большее основание  x:");
+            x = double.Parse(Console.ReadLine());
+
+
+
+            double y;
+            Console.WriteLine("Введите меньшее основание  y:");
+            y = double.Parse(Console.ReadLine());
+
+
+            double z;
+            Console.WriteLine("Введите высоту z:");
+            z = double.Parse(Console.ReadLine());
+
+
+            double s;
+            s = ((z / 2) + (x + y));
+
+            s = Math.Round(s, 3);
+
+            Console.WriteLine("S = " + s);
+
+            double t;
+
+            t = (x - y) / 2;
+
+            double q;
+
+            q = Math.Sqrt(Math.Pow(z, 2) + Math.Pow(t, 2));
+
+            q = Math.Round(q, 3);
+
+
+            double p;
+            p = x + y + q + q;
+
+            p = Math.Round(p, 3);
+
+            Console.WriteLine("P = " + p);
+
+
+
+
+
+
+
+
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
---
+++ Целочисленная арифметика/Целочисленная арифметика/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Целочисленная_арифметика
+{
+    class Program
+
+    {
+
+        static void Main(string[] args)
+
+        {
+          
+            Console.WriteLine("Введите четырехзначное число: ");
+
+
+
+            var n = Convert.ToInt32(Console.ReadLine());
+            n = n % 100 * 1000 + n / 1000 % 1000 * 100 + n / 100 % 100 + n / 10 % 10;
+
+            Console.WriteLine(n);
+
+            Console.ReadLine();
+
+            Console.ReadKey();
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+    }
+
