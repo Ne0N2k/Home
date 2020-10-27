7551294ecc751d276a78b26b08e25ad54cb2e8f4
Ne0N2k <tarasov-kola@mail.ru>
2020-10-27 22:22:54 +0500

    Полученное слово

--- WordCreate/WordCreate/Program.cs
+++ WordCreate/WordCreate/Program.cs
@@ -4,14 +4,15 @@ namespace WordCreate
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Первоначальное слово: Коронация ");
-            string str = "Ц" + "и" + "р" + "к";
-            System.Console.WriteLine("Первое полученное слово: " + str);
+            string word = "коронация";
 
-            string Newstr = "О" + "к" + "о" + "р" + "о" + "к";
-            System.Console.WriteLine("Второе полученное слово: " + Newstr);
+            string word1 = word.Substring(6, 1) + word.Substring(7, 1) + word.Substring(2,1) + word.Substring(0,1);
+            Console.WriteLine("Первое слово: " + word1);
+
+            string word2 = word.Substring(1, 1) + word.Substring(0, 1) + word.Substring(3, 1) + word.Substring(2, 1) + word.Substring(3, 1) + word.Substring(0, 1);
+            Console.WriteLine("Второе слово: " + word2);
             Console.ReadKey();
         }
     }
