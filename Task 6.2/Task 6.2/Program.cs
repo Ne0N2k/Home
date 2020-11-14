94d51c102c1e3f0f07ebe540ef68a4df28c94e6e
Ne0N2k <tarasov-kola@mail.ru>
2020-11-14 22:14:50 +0500

    Task6.2

---
+++ Task 6.2/Task 6.2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Task_6._2
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Введите координату x: ");
+            var x = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите координату y: ");
+            var y = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Принадлежит ли точка области? - " + PointInArea(x, y));
+
+            Console.ReadKey();
+        }
+
+        static bool PointInArea(double x, double y)
+        {
+            return  -2 <= y && y <= 1.5; 
+                   
+              
+        }
+
+    }
+}
