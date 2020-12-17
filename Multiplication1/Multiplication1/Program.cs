2b6f890e7c42c0944a9772e51d64e7fc9ded81f9
Ne0N2k <tarasov-kola@mail.ru>
2020-12-17 15:11:40 +0500

    Операторы цикла 12.1

---
+++ Multiplication1/Multiplication1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Multiplication1
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Введите m: ");
+            var m = double.Parse(Console.ReadLine());
+            Console.WriteLine("Введите n: ");
+            var n = double.Parse(Console.ReadLine());
+            Console.WriteLine("Произведение = " + Operator(m, n) + "(Если вы получили 0, введите данные корректно)");
+            Console.ReadKey();
+        }
+ 
+        public static double Operator(double m, double n)
+        {
+            if (1 <= m && m <= n && n < 20) 
+            {
+                return n*m ;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
+
+}
