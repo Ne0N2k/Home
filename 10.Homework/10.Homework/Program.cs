f9b94edae4fd15a405d0bb061364d94e57f5d5bf
Ne0N2k <tarasov-kola@mail.ru>
2020-12-28 00:20:12 +0500

    Многомерные массивы 12 вариант

---
+++ 10.Homework/10.Homework/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+
+namespace _10.Homework
+{
+    class Program
+    {
+        static void Main()
+        {
+            int a = 0, b = 0;
+
+            var tryAgain = true;
+
+            while (tryAgain)
+            {
+                Console.WriteLine("Введите число строк (a)");
+                var input = Console.ReadLine();
+
+                if (int.TryParse(input, out a) && a >= 5 && a <= 20)
+                {
+                    while (true)
+                    {
+                        Console.WriteLine("Введите число столбцов (b)");
+                        input = Console.ReadLine();
+
+                        if (int.TryParse(input, out b) && b >= 5 && b <= 20)
+                        {
+                            tryAgain = false;
+                            break;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Ошибка ввода\b");
+                            continue;
+                        }
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Ошибка ввода\b");
+                    continue;
+                }
+            }
+
+            var rnd = new Random();
+
+            var table = new int[a, b];
+
+            for (var i = 0; i < table.GetLength(0); i++)
+                for (var f = 0; f < table.GetLength(1); f++)
+                    table[i, f] = rnd.Next(0, 100);
+
+            PrintTable(table);
+
+            Console.WriteLine();
+
+            GetElementsInDescendingOrder(table);
+
+            Console.WriteLine();
+
+            GetDifferenceOfTheSums(table);
+
+            Console.ReadKey();
+        }
+
+        static void PrintTable(int[,] q)
+        {
+            for (var i = 0; i < q.GetLength(0); i++)
+            {
+                for (var f = 0; f < q.GetLength(1); f++)
+                    Console.Write($"{q[i, f],2} ");
+
+                Console.WriteLine();
+            }
+        }
+
+        static void GetElementsInDescendingOrder(int[,] q)
+        {
+            for (int i = 0; i < q.GetLength(0); i++)
+            {
+                for (int f = 0; f < q.GetLength(1); f++)
+                {
+                    if (f > 0)
+                    {
+                        if (q[i, f] >= q[i, f - 1])
+                        {
+                            Console.Write($"{q[i, f],2} ");
+                        }
+                    }
+                }
+
+                Console.WriteLine();
+            }
+        }
+
+        static void GetDifferenceOfTheSums(int[,] q)
+        {
+            int First = 0;
+            int Second = 0;
+            for (int i = 0; i < q.GetLength(0); i++)
+            {
+                for (int f = 0; f < q.GetLength(1); f++)
+                {
+                    if (f % 2 == 0)
+                        First = First + q[i, f];
+                    else
+                        Second = Second + q[i, f];
+                }
+
+                Console.WriteLine($"{i,2}: {First - Second,3} ");
+            }
+        }
+    }
+}
+   
---
+++ 8.3.Number/8.3.Number/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _8._3.Number
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Введите значение a: ");
+            var a = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Значение функции равно: " + ConditionalOperator(a));
+
+            Console.ReadKey();
+        }
+        public static double ConditionalOperator(double x)
+        {
+            if (Math.Abs(x) > 1)
+            {
+                return 1;
+            }
+            else
+            {
+                if (0 < Math.Abs(x) && Math.Abs(x) <= 1)
+                {
+                    return Math.Abs(x);
+                }
+                else
+                {
+                    return 2;
+                }
+            }
+        }
+    }
+}
